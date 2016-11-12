9f785d903189aa8055ce0f72e05cec273e09a68f
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-11 23:54:04 -0500

    Moved state into app

--- components/App.js
+++ components/App.js
@@ -2,10 +2,34 @@ const React = require('react');
 
 const FruitBasket = require('./FruitBasket');
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+// const App = () => {
+//   return (
+//     <FruitBasket />
+//   );
+// }
+
+class App extends React.Component{
+  constructor(){
+    super();
+    
+    this.state = {
+      filters: [],
+      selectedFilter: null,
+      fruit: []
+    }
+  }
+  
+  componentWillMount() {
+    fetch('/api/fruit')
+      .then(res => res.json())
+      .then(fruit => this.setState({ fruit: fruit}));
+  }
+  
+  render(){
+    return(
+      <FruitBasket />
+    )
+  }
 }
 
 module.exports = App;
